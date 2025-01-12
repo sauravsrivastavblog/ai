4b635ab11e02ac9937d9e294300847fbd30769da
Saurav Srivastav <Sauravsrivastav2205@gmail.com>
2025-01-12 23:38:47 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sauravsrivastavblog.github.io',
-	base: '/blog',
+	base: '/ai',
 	integrations: [mdx(), sitemap()],
 });
